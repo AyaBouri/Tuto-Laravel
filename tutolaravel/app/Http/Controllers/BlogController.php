c8217b5f3236a4b08d3bb12b4cf56f4c653ee29f
Aya Bouri <aya.bouri1@gmail.com>
2024-02-02 16:03:12 +0100

    update

--- tutolaravel/app/Http/Controllers/BlogController.php
+++ tutolaravel/app/Http/Controllers/BlogController.php
@@ -1,24 +1,22 @@
 <?php
 namespace App\Http\Controllers;
+use App\Http\Requests\BlogFilterRequest;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Validator;
 class BlogController extends Controller
 {
-    public function index():View{
-        $validator=Validator::make([
-            'title'=>'',
-        ],[
-            'title'=>'required | min:8'
-        ]);
-        dd($validator->errors());
+    public function index(BlogFilterRequest $request):View{
+        //dd($request->validated());
         return view('blog.index',[
             'posts' => Post::paginate(1),
         ]);
     }
-    public function show(string $slug,string $id):RedirectResponse|View{
-        $post=Post::findOrFail($id);
+    public function show(string $slug,Post $post):RedirectResponse|View{
+        dd($post);
+        //$post=Post::findOrFail($post);
         if($post->slug != $slug){
             return to_route('blog.show',['slug'=>$post->slug,'id'=>$post->id]);
         }
--- tutolaravel/routes/web.php
+++ tutolaravel/routes/web.php
@@ -19,7 +19,7 @@ Route::prefix('/blog')->name('blog.')->controller(BlogController::class)->group(
     //accueil
     Route::get('/','index')->name('index');
 //list
-    Route::get('/{slug}-{id}','show')->where([
+    Route::get('/{slug}-{post}','show')->where([
         'id'=>'[0-9]+',
         'slug'=>'[a-z0-9\-]+'
     ])->name('show');

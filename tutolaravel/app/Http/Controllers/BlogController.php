3c44fccf7cb62e4e41f6291b70e8e73b605cde81
Aya Bouri <aya.bouri1@gmail.com>
2024-02-02 13:17:28 +0100

    update

--- tutolaravel/app/Http/Controllers/BlogController.php
+++ tutolaravel/app/Http/Controllers/BlogController.php
@@ -3,10 +3,12 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 class BlogController extends Controller
 {
-    public function index():Paginator{
-        return Post::paginate(25);
+    public function index():View{
+        $posts= Post::paginate(25);
+        return view('blog.index');
     }
     public function show(string $slug,string $id):RedirectResponse|Post{
         $post=Post::findOrFail($id);

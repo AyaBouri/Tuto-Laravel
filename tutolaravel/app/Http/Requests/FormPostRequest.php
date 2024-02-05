db7d0573731d2027327097ae49dd8466148477fb
Aya Bouri <aya.bouri1@gmail.com>
2024-02-05 14:17:38 +0100

    update

--- tutolaravel/app/Http/Requests/FormPostRequest.php
+++ tutolaravel/app/Http/Requests/FormPostRequest.php
@@ -1,10 +1,9 @@
 <?php
 namespace App\Http\Requests;
-use \Illuminate\Support\Str;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
-
-class CreateRequest extends FormRequest
+class FormPostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,11 +21,10 @@ class CreateRequest extends FormRequest
     {
         return [
             'title'=>['required','min:8'],
-            'slug'=>['required','min:8','regex:/^[0-9a-z\-]+$/',Rule::unique('posts')],
+            'slug'=>['required','min:8','regex:/^[0-9a-z\-]+$/',Rule::unique('posts')->ignore($this->route()->parameter('post'))],
             'content'=>['required']
         ];
     }
-    //19:24
     protected function prepareForValidation(){
         $this->merge([
             'slug'=>$this->input('slug') ?: Str::slug($this->input('title'))
